d9862ae0139c8a2812fde55103bb6d90006ca5df
Navaneeth joshi <neeth35@gmail.com>
2024-03-12 10:36:19 +0000

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,14 +1,18 @@
 import os
+import sys
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from src.logger import logging  # Assuming your logging setup is correct
 from src.exception import CustomException
+from src.logger import logging
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig  # Import DataTransformation here
+
+@dataclass
 class DataIngestionConfig:
-    def __init__(self):
-        self.train_data_path = os.path.join('artifacts', 'train.csv')
-        self.test_data_path = os.path.join('artifacts', 'test.csv')
-        self.raw_data_path = os.path.join('artifacts', 'data.csv')
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -17,8 +21,8 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv(os.path.join('notebook', 'data', 'stud.csv'))
-            logging.info('Read the dataset as a dataframe')
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
@@ -28,15 +32,22 @@ class DataIngestion:
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Ingestion of the data is completed")
 
-            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
         except Exception as e:
-            logging.exception("An error occurred during data ingestion")
-            raise CustomException(e, sys.exc_info()[2])
+            raise CustomException(e, sys)
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    # Instantiate DataTransformation here
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
